36de55460e976ea3f06ef4c26e0cb05c543fa517
Rafael Galvao <rafaelgalvaolm.dev@gmail.com>
2023-01-23 17:01:52 -0300

    rafael: enviando o script modificado, bem como, o procedure

---
+++ SP_crud_vendas.sql
@@ -0,0 +1,160 @@
+--------------------------------------------SELECT-----------------------------------------------------------------------------------------
+IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.SelVenda]') AND objectproperty(ID,N'isProcedure') = 1)
+	DROP PROCEDURE [SP.SelVenda]
+
+GO
+
+CREATE PROCEDURE [SP.SelVenda]
+	@id int = NULL,
+
+	AS
+
+	/* Documentação
+	Arquivo Fonte..........: venda.sql
+	Objetivo...............: Selecionar um ou mais registros da tabela venda
+	Autor..................: Rafael Galvão
+	Data...................: 23/01/2023
+	Ex.....................: EXEC [SP.SelVenda] @id = 1
+	*/
+	BEGIN
+
+    
+
+		SELECT id, 
+			   funcionario_id, 
+			   produto_id,
+			   data_venda
+			FROM dbo.venda
+			WHERE id = COALESCE(@id, id)
+    
+	END
+GO
+
+---------------------------------------------INSERT-----------------------------------------------------------------------------------------
+
+ IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.InsVenda]') AND objectproperty(ID,'isProcedure') = 1)
+		DROP PROCEDURE [SP.InsVenda]
+
+GO
+
+CREATE PROCEDURE [SP.InsVenda]
+	@funcionario_id        int,
+	@produto_id			   int,
+	@data_venda            datetime
+
+	AS
+
+	/* Documentação
+	Arquivo Fonte ........: Venda.sql
+	Objetivo..............: Inserir um ou mais dados dentro da tabela venda
+	Autor.................: Rafael Galvão
+	Data..................: 23/01/2023
+	EX....................: EXEC [SP.InsVenda] @funcionario_id = 3, @produto_id = 23, @data_venda = '20230113 20:00:56'
+	código de retorno.....: 0 - Execução OK
+                            1 - Execução Falhou
+	*/
+
+	BEGIN
+
+		INSERT INTO dbo.venda (funcionario_id, produto_id, data_venda)
+			VALUES (@funcionario_id, @produto_id, @data_venda)
+
+		IF @@ERROR <> 0
+			RETURN 1
+
+		RETURN 0
+	END
+
+GO
+
+---------------------------------------------UPDATE-----------------------------------------------------------------------------------------
+ 
+ IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.UpVenda]') AND objectproperty(ID,'isProcedure') = 1)
+		DROP PROCEDURE [SP.UpVenda]
+
+ GO
+
+ CREATE PROCEDURE [SP.UpVenda]
+
+	@id                    int,
+	@funcionario_id        int,
+	@produto_id			   int,
+	@data_venda            datetime
+	
+	AS
+
+        /* Documentação
+        Arquivo Fonte ......: Venda.sql
+        Objetivo ...........: Atualizar um ou mais registros na tabela venda
+        Autor ..............: Rafael Galvão
+        Data ...............: 23/01/2023
+        Ex .................: EXEC [SP.UpVenda] @id = 10, @funcionario_id = 5, @produto_id = 40, @data_venda = '20230110 13:12:32'
+        Códigos de retorno: 0 - excução ok
+                            1 - id não encontrado
+        */
+
+	BEGIN		
+
+		UPDATE dbo.venda
+		SET  funcionario_id = @funcionario_id,
+			 produto_id = @produto_id,
+			 data_venda = @data_venda
+	   WHERE id = @id
+
+	IF @@ROWCOUNT = 0
+		RETURN 1
+
+		RETURN 0 
+	END
+GO
+
+--------------------------------------------DELETE-----------------------------------------------------------------------------------------
+ 
+ IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.DelVenda]') AND objectproperty(ID,'isProcedure') = 1)
+		DROP PROCEDURE [SP.DelVenda]
+
+GO
+
+	CREATE PROCEDURE [SP.DelVenda]
+		@id			        int,
+        @funcionario_id     int,
+        @produto_id         int
+
+	AS
+
+    /* Documentação
+    Arquivo Fonte...........: Venda.sql
+    Objetivo................: Deletar um registro da tabela venda
+    Autor...................: Rafael Galvão
+    Data....................: 23/01/2023
+    Ex......................: declare @cod int 
+                            EXEC @cod = [SP.DelVenda] @id = 1
+                            select @cod
+        Código de Retorno:  0 - Execuçao OK
+                            1 - id não encontrado
+                            2 - esse id esta sendo mencionado na tabela Funcionario
+                            3 - esse id esta sendo mencionado na tabela Produto
+    */	
+
+
+	BEGIN
+		DECLARE @DATEINT DATETIME = GETDATE()
+		DELETE FROM dbo.venda
+			WHERE id = @id
+
+		IF @@ROWCOUNT = 0
+				 RETURN 1
+
+        ELSE IF @funcionario_id EXISTS 
+                 RETURN 2
+
+        ELSE IF @produto_id EXISTS
+                 RETURN 3
+
+        ELSE
+
+		RETURN DATEDIFF(MILLISECOND, @DATEINT, GETDATE())
+
+		END
+	GO
+
--- ScriptCreate.sql
+++ ScriptCreate.sql
@@ -1,8 +1,19 @@
 CREATE TABLE IF NOT EXISTS cliente(
     id INT PRIMARY KEY IDENTITY(1,1),
     nome VARCHAR(100) NOT NULL, 
-    endereco VARCHAR(80) NOT NULL, 
-    telefone VARCHAR(12) NOT NULL, 
+    telefone BIGINT NOT NULL, 
+);
+
+CREATE TABLE IF NOT EXISTS endereco(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    cliente_id INT NOT NULL,
+    nome_rua VARCHAR(80) NOT NULL, 
+    numero SMALLINT,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(60) NOT NULL,
+    estado VARCHAR(30) NOT NULL,
+    CONSTRAINT fk_cliente_id1 FOREIGN KEY (cliente_id) 
+        REFERENCES cliente (id)
 );
 
 CREATE TABLE IF NOT EXISTS funcionario( 
@@ -23,13 +34,19 @@ CREATE TABLE IF NOT EXISTS produto(
     nome VARCHAR(60) NOT NULL, 
     descricao VARCHAR(240) NOT NULL,
     preco DECIMAL(7,2) NOT NULL
+    CONSTRAINT fk_categoria_id FOREIGN KEY (categoria_id)
+        REFERENCES categoria(id)
 );
 
 CREATE TABLE IF NOT EXISTS venda( 
     id INT PRIMARY KEY IDENTITY(1,1),
     funcionario_id INT NOT NULL, 
     produto_id INT NOT NULL, 
-    data_venda DATETIME NOT NULL, 
+    data_venda DATETIME NOT NULL,
+    CONSTRAINT fk_funcionario_id  FOREIGN KEY (funcionario_id)
+        REFERENCES funcionario(id),
+    CONSTRAINT fk_produto_id FOREIGN KEY (produto_id)
+        REFERENCES produto(id) 
 ); 
 
 CREATE TABLE IF NOT EXISTS pedido( 
@@ -39,4 +56,8 @@ CREATE TABLE IF NOT EXISTS pedido(
     data_pedido DATE NOT NULL, 
     quantidade INT NOT NULL, 
     preco DECIMAL(7,2) NOT NULL
+    CONSTRAINT fk_cliente_id FOREIGN KEY (cliente_id)
+        REFERENCES cliente(id),
+    CONSTRAINT fk_produto_id1 FOREIGN KEY (produto_id)
+        REFERENCES produto(id)
 );
